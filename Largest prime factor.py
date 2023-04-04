77ea3c36f759dfc1e0f560a79b5fd21849b8857d
Alex Preston <apreston@smu.edu>
2023-04-03 23:21:09 -0400

    Problem 3

--- Largest prime factor.py
+++ Largest prime factor.py
@@ -1,16 +1,23 @@
+currNum = 600851475143
+
+def isPrime(num) -> bool:
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
 
 
+j = 2
+results = []
 
+while True:
+    if j == currNum:
+        results.append(j)
+        break
+    if currNum%j == 0 and isPrime(j):
+        results.append(j)
+        currNum /= j
+        j = 2
+    j += 1
+print(results)
 
-def primeFactor(num, primeSum):
-    for i in range(num,2, -1):
-        if num%i == 0:
-            primeSum += i
-            print(i)
-            primeFactor(num//i, primeSum)
-            
-    return primeSum
-        
-
-
-print(primeFactor(13195, 0))
